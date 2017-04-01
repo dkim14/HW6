1753b0e9c62dc1e66b5767dfa1017f376d5b0662
Dowan Kim <dkim14@uiowa.edu>
2017-04-01 03:05:21 -0500

    UpdateQuery made

---
+++ src/java/dbHelpers/UpdateQuery.java
@@ -0,0 +1,69 @@
+package dbHelpers;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.Pokemons;
+
+public class UpdateQuery {
+    
+    private Connection conn;
+    
+    public UpdateQuery(){
+         Properties props = new Properties();//MWC
+        InputStream instr = getClass().getResourceAsStream("dbConn.properties");
+        try {
+            props.load(instr);
+        } catch (IOException ex) {
+            Logger.getLogger(UpdateQuery.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+            instr.close();
+        } catch (IOException ex) {
+            Logger.getLogger(UpdateQuery.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        
+        String driver = props.getProperty("driver.name");
+        String url = props.getProperty("server.name");
+        String username = props.getProperty("user.name");
+        String passwd = props.getProperty("user.password");
+        try {
+            Class.forName(driver);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(UpdateQuery.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+            conn = DriverManager.getConnection(url, username, passwd);
+        } catch (SQLException ex) {
+            Logger.getLogger(UpdateQuery.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    
+    }
+    
+    public void doUpdate (Pokemons pokemon) {
+    
+        try {
+            String query = "UPDATE pokemons SET pokemonName =?, pokemonType=?, pokemonNumber=? WHERE pokemonID=?";
+            
+            PreparedStatement ps = conn.prepareStatement(query);
+            
+            ps.setString(1, pokemon.getPokemonName());
+            ps.setString(2, pokemon.getPokemonType());
+            ps.setInt(3, pokemon.getPokemonNumber());
+            ps.setInt(4,pokemon.getPokemonID());
+            
+            ps.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(UpdateQuery.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+}
